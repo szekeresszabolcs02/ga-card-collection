97640de755b74e308608b02cb0b8d9cb7067b46d
szekeresszabolcs02 <szekeres.szabolcs02@gmail.com>
2026-02-18 21:36:53 +0000

    Add files via upload

--- gacc.js
+++ gacc.js
@@ -16,18 +16,23 @@ import {
 
 /*Firebase Config------------*/
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "IDE_A_VALODI_API_KEY",
+  apiKey: "AIzaSyBd--_OmOvCXTfuAQ-D96IS6NgRssCMavg",
   authDomain: "grayson-card-collection.firebaseapp.com",
   projectId: "grayson-card-collection",
-  storageBucket: "grayson-card-collection.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
+  storageBucket: "grayson-card-collection.firebasestorage.app",
+  messagingSenderId: "868175520778",
+  appId: "1:868175520778:web:0cabbf5d88912c35aaabf3"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
 /*--------------------- idaig */
 
 /*Anonymus login innen*/
